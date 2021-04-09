28f2c98abbcb639bb22b2f2f384c05cc564e9109
wananshine <wananshine@hotmail.com>
2021-04-09 18:07:48 +0800

    update

--- HonHai/React +Typescript/react+dva/src/routes/Test/components/index.js
+++ HonHai/React +Typescript/react+dva/src/routes/Test/components/index.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
-import { connect, router } from 'dva';
 import PropTypes from 'prop-types';
 import { Table, Popconfirm, Button } from 'antd';
 
 
-const Test = () => {
+const Test = (props) => {
+  console.log(123, props);
   return (
     <div>
       空白页
-      <Button>Delete{state}</Button>
+      <Button>Delete</Button>
     </div>
   );
 };
 
-//这里的example表示后面用this.props.example获取state（根节点）中exmpale命名空间（model的example.js中的state所有数据）的数据
-const mapStateToProps = (state) =>{
-    return {
-        blank: state.blank,
-    }
-}
 
-//通过这种方式来把model层的数据传递到当前组件了
-// @connect(({ blank }) =>(blank))
-// export default Test;
-
-
-export default connect(mapStateToProps)(Test)
+export default Test
--- HonHai/React +Typescript/react+dva/src/routes/Test/index.js
+++ HonHai/React +Typescript/react+dva/src/routes/Test/index.js
@@ -1,6 +1,14 @@
-import blank from './components'
-import model from './model';
-console.log(model)
+import { connect, router } from 'dva';
+import Blank from './components'
+import Model from './model';
 
 
-export default blank;
+//这里的example表示后面用this.props.example获取state（根节点）中exmpale命名空间（model的example.js中的state所有数据）的数据
+const mapStateToProps = (state) => Model
+
+
+//通过这种方式来把model层的数据传递到当前组件了
+// @connect(({ blank }) =>(blank))
+// export default Test;
+
+export default connect(mapStateToProps)(Blank);
